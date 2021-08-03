f6f4f436c6829f2a6f90b8c381f73da0dc0bd876
ElusiveEntity <30778577+ElusiveEntity@users.noreply.github.com>
2021-08-03 18:05:16 -0400

    Began implementation of index.js (2)

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 const questions = require('./src/questions');
+const { inherits } = require("util");
 
 let employeeList = [];
 
-inquirer.prompt(
-    questions.promptManager()
-)
-.then(
-    (answers) => {
-        console.log(answers)
+async function initialize() {
+    let manager = await inquirer.prompt(questions.promptManager())
+    employeeList.push(manager)
+
+    let employeeListComplete = false;
+    while (employeeListComplete === false) {
+        let employee = await inquirer.prompt(questions.promptEmployeeSelector())
+
+        switch (employee) {
+            case 'Engineer':
+                
+                break;
+        
+            default:
+                break;
+        }
     }
-)
+
+}
+
+initialize()
